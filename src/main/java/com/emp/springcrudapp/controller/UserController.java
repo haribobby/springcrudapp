668af30195ab85c3df65821bf89eac8b5b691dbe
HariBobby <hari.polnati@gmail.com>
2018-05-21 09:16:51 +0530

    UserController -> UserById endpoint added

--- src/main/java/com/emp/springcrudapp/controller/UserController.java
+++ src/main/java/com/emp/springcrudapp/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +36,10 @@ public class UserController {
 	@GetMapping
 	public ResponseEntity getAllUsers() {
 		return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity getUserById(@PathVariable("id") Long id) {
+		return null;
 	}
 }
