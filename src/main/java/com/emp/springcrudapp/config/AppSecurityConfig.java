510ce1958e8b10bca652fa4edc3b75bae34ed891
HariBobby <hari.polnati@gmail.com>
2018-05-22 07:58:23 +0530

    Inmemeroy Security enabled to my application

--- src/main/java/com/emp/springcrudapp/config/AppSecurityConfig.java
+++ src/main/java/com/emp/springcrudapp/config/AppSecurityConfig.java
@@ -1,39 +1,39 @@
-/*package com.emp.springcrudapp.config;
+package com.emp.springcrudapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
-//@EnableWebSecurity
+@EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/*").hasRole("USER").and().httpBasic();
-		
-		http
-		.authorizeRequests()
-			.antMatchers("/css/**", "/index").permitAll()		
-			.antMatchers("/user/**").hasRole("USER")			
-			.and()
-		.formLogin()
-			.loginPage("/login").failureUrl("/login-error");
-	}
-
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("mahesh").password("mahesh@123").roles("USER").and().withUser("raj")
-				.password("raj@123").roles("USER");
+		auth.inMemoryAuthentication()
+				.passwordEncoder(NoOpPasswordEncoder.getInstance())
+				.withUser("mahesh").password("test").roles("USER").and().withUser("raj").password("test2")
+				.roles("ADMIN");
 	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+		httpSecurity.authorizeRequests().anyRequest().fullyAuthenticated()
+				// .antMatchers("**/rest/*")
+				.and()
+				// .addFilterBefore(customFilter(), BasicAuthenticationFilter.class)
+				.httpBasic();
+		httpSecurity.csrf().disable();
+
 	}
+
+//	@Bean
+//	public BCryptPasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder ();
+//	}
 
 }
-*/
