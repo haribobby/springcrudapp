6a15aafe52c58b2dacfb9b9de027103fce0495d5
HariBobby <hari.polnati@gmail.com>
2018-05-23 07:59:47 +0530

    commit message

--- src/main/java/com/emp/springcrudapp/SpringcrudappApplication.java
+++ src/main/java/com/emp/springcrudapp/SpringcrudappApplication.java
@@ -3,7 +3,7 @@ package com.emp.springcrudapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*
- * @author Hari
+ * @author Hari123
  */
 @SpringBootApplication
 public class SpringcrudappApplication {
