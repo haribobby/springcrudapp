fc53fc52b21b38290721ce1a3a2c4f1b6c0e37a0
HariBobby <hari.venkatesh2@gmail.com>
2018-05-23 07:59:23 +0530

    Update UserController.java

--- src/main/java/com/emp/springcrudapp/controller/UserController.java
+++ src/main/java/com/emp/springcrudapp/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.emp.springcrudapp.model.User;
 import com.emp.springcrudapp.service.UserService;
 /*
-@author Mahesh
+@author Mahesh123
 */
 @RestController
 @RequestMapping("/user")
